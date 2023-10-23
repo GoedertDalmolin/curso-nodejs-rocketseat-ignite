a078b989f811206d75671d6b2682939b06a47944
GoedertDalmolin <gbrunodalmolin@gmail.com>
2023-10-23 16:57:03 -0300

    Compreendido como obter a url e o método usado na request pelo cliente.

--- 01-fundamentos-nodejs/src/server.js
+++ 01-fundamentos-nodejs/src/server.js
@@ -1,6 +1,18 @@
 import http from 'node:http'
 
 const server = http.createServer((req, response) => {
+    const { method, url } = req;
+
+    console.log(method, url)
+
+    if (method == 'GET' && url == '/users') {
+        return response.end('Listagem de usuarios')
+    }
+
+    if (method == 'POST' && url =='/users') {
+        return response.end('Criacão de usuarios')
+    }
+    
     return response.end('Hello World')
 })
 
