c362ea6cca541477b71dbc513f8aaa626cb146b7
GoedertDalmolin <gbrunodalmolin@gmail.com>
2023-10-23 17:50:48 -0300

    // Visto sobre o conceito de streams. // Entendido a diferença entre readable streams e writable streams.

---
+++ 01-fundamentos-nodejs/streams/fundamentals.js
@@ -0,0 +1,5 @@
+// Visto sobre o conceito de streams.
+// Entendido a diferença entre readable streams e writable streams.
+
+// Netflix & Spotify - readable streams
+// Importação e gravação de dados CSV no banco de dados - Writable Streams.
