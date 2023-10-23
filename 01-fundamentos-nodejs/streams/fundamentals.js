25cdfc9ac061ddf1419ba68a56f02826a594b4e2
GoedertDalmolin <gbrunodalmolin@gmail.com>
2023-10-23 19:12:05 -0300

    Visto sobre o conceito de Stream Writeble na pratica. O process.stdout é uma stream que processa dados e não que le dados. Visto sobre o conceito de Stream Transform na pratica. Visto sobre a teoria do conceito Stream duplex.

--- 01-fundamentos-nodejs/streams/fundamentals.js
+++ 01-fundamentos-nodejs/streams/fundamentals.js
@@ -1,5 +1,5 @@
 import process from 'node:process'
-import { Readable } from 'node:stream'
+import { Readable, Writable, Transform} from 'node:stream'
 
 class OneToHundredStream extends Readable {
     index = 1
@@ -22,5 +22,31 @@ class OneToHundredStream extends Readable {
     }
 }
 
-new OneToHundredStream().pipe(process.stdout)
+// new OneToHundredStream().pipe(process.stdout)
 
+class  MultiplByTenStream extends Writable {
+    _write(chunk, encoding, callback) {
+        console.log(Number(chunk.toString()) * 10)
+        callback()
+    }
+}
+
+// new OneToHundredStream().pipe(new MultiplByTenStream())
+
+class InverseNumberStream extends Transform {
+    _transform(chunk, encoding, callback) {
+        const transformed = Number(chunk.toString()) * -1
+        const buf = Buffer.from(String(transformed));
+
+        callback(null, buf)
+    }
+}
+
+new OneToHundredStream()
+.pipe(new InverseNumberStream())
+.pipe(new MultiplByTenStream())
+
+// Visto sobre o conceito de Stream Writeble na pratica.
+// O process.stdout é uma stream que processa dados e não que le dados.
+// Visto sobre o conceito de Stream Transform na pratica.
+// Visto sobre a teoria do conceito Stream duplex.
