250f18a22f8388d1ff69e9950397085a86706b94
GoedertDalmolin <gbrunodalmolin@gmail.com>
2023-10-27 18:30:41 -0300

    Visto como obter o body da request e  converter para JSON. Tudo isso utilizando streams.

--- 01-fundamentos-nodejs/src/server.js
+++ 01-fundamentos-nodejs/src/server.js
@@ -2,9 +2,22 @@ import http from 'node:http'
 
 const users = []
 
-const server = http.createServer((req, response) => {
+const server = http.createServer(async (req, response) => {
     const { method, url } = req;
 
+    const buffers = [];
+
+    for await (const chunk of req) {
+        buffers.push(chunk);
+    }
+
+
+    try {
+        req.body = JSON.parse(Buffer.concat(buffers).toString());
+    } catch {
+        req.body = null;
+    }
+    
     console.log(method, url)
 
     if (method == 'GET' && url == '/users') {
@@ -14,10 +27,12 @@ const server = http.createServer((req, response) => {
     }
 
     if (method == 'POST' && url == '/users') {
+        const { nome, email } = req.body
+
         users.push({
             id: 1,
-            name: 'John Doe',
-            email: 'johndoe@example.com',
+            name: nome,
+            email: email,
         })
 
         // Status 201 simboliza que a request foi um sucesso, porém que especificamente foi possivel criar um recurso.
